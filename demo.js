00c18332430978bdc95a41691025078dcec6a0af
Rodrigo Pacheco Curro <rodrigo.pachecocurro@gmail.com>
2017-05-26 20:43:21 -0400

    customPredict parsing predictions correctly

--- demo.js
+++ demo.js
@@ -4,11 +4,11 @@ console.log("Going to request agency");
 ru.cacheAgency("rutgers", 40.4, 40.6, function(err) {
   console.log("Got agency");
   var routeStops = [
-    {routeTag: "b", stop: "Hill Center"},
+    {routeTag: "wknd1", stop: "Hill Center"},
   ];
   ru.customPredict(routeStops, function(err, data) {
     console.log("got custom predict");
-    console.log(err);
     console.log(data);
+    console.log(err);
   });
 });
