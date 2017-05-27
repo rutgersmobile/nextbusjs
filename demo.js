0dc0fb7621fe15f15ea07bad4eb74ca52bab8f51
Rodrigo Pacheco Curro <rodrigo.pachecocurro@gmail.com>
2017-05-27 09:27:23 -0400

    Adds ability to look up (in customPredict) by route title or route tag

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ console.log("Going to request agency");
 ru.cacheAgency("rutgers", 40.4, 40.6, function(err) {
   console.log("Got agency");
   var routeStops = [
-    {routeTag: "wknd1", stop: "Hill Center"},
+    {route: "Weekend 1", stop: "Hill Center"},
   ];
   ru.customPredict(routeStops, function(err, data) {
     console.log("got custom predict");
